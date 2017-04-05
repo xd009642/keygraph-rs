d4895bec3866b4dc2a6224cd9c490e84afaed46d
Daniel McKenna <danielmckenna93@gmail.com>
2017-04-05 23:33:20 +0100

    Finished coveralls integration

--- generate_coverage.py
+++ generate_coverage.py
@@ -14,7 +14,7 @@ binaries = ['target/debug/'+f for f in listdir("target/debug/") if regex.match(f
 
 targets = []
 for i, binary in enumerate(binaries):
-    system('kcov --exclude-pattern=/.cargo,/usr/lib --verify target/cov' + str(i) +' '+binary)
+    system('kcov --exclude-pattern=/.cargo,/usr/lib,tests --verify target/cov' + str(i) +' '+binary)
     targets.append('target/cov' + str(i))
 
 system('kcov --coveralls-id=$repo_token --merge target/cov ' + ' '.join(targets))
